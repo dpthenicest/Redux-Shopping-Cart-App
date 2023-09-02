6bdc132a18f6a7ac2f4544bb03b3e1d332edc39c
Fortune Precious <fortuneprecious17@gmail.com>
2023-09-02 23:59:39 +0100

    added auth-slice and store components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
   return (
     <div className="App">
       <Auth />
